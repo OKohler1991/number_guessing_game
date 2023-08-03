fe5e0aea21291aab19b883bd1fe7f5c3709c93c5
Oliver Kohler <oliver_me_kohler@pm.me>
2023-08-03 21:22:28 +0200

    full project

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def number_in_range(number):
     while True:
         try:
             number = int(number)
-            if number not in range(number_lower, number_higher + 1):
+            if not (number_lower <= number <= number_higher):
                 raise NumberOutOfRangeException
         except ValueError:
             number = input(
@@ -33,20 +33,29 @@ def number_in_range(number):
 
 def main():
     while True:
+        game = "on"
         chosen_number = guess_number()
         print(chosen_number)
-        player_number = input(
-            f"Please enter a number between {number_lower} and {number_higher}: "
-        )
-        number = number_in_range(player_number)
-        if number == chosen_number:
-            pass
-            # TODO: Text + question to play a new game or end game
-        else:
-            if number < chosen_number:
-                pass
+        while game == "on":
+            player_number = input(
+                f"Please enter a number between {number_lower} and {number_higher}: "
+            )
+            number = number_in_range(player_number)
+            if number == chosen_number:
+                print(
+                    "Congratulation, you guessed the right number, do you want to play another round?"
+                )
+                player_input = input("Press 'Y' for yes or 'N' for no: ")
+                if player_input == "Y":
+                    game = "off"
+                else:
+                    print("Have a nice day!")
+                    return False
             else:
-                pass
+                if number < chosen_number:
+                    print("You're guessed number is lower than the searched number")
+                else:
+                    print("You're guessed number is higher than the searched number")
 
 
 if __name__ == "__main__":
